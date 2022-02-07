c59767170b8e832fad86afc15bd7c7964d6f6aca
Fyodor_Ishch <f.ishhenko556@mail.ru>
2022-02-07 18:24:15 +0300

    Вывод поиска

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+import sys
+import os
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('zadacha_design.ui', self)
+        self.pushButton_4.clicked.connect(self.search)
+
+    def search(self):
+        zapros = f"https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={self.lineEdit.text()}1&format=json"
+        response = requests.get(zapros)
+        if response:
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"][
+                                  "text"] + ", Государственный исторический музей Москвы"
+            toponym_coodrinates = toponym["Point"]["pos"]
+            print(toponym_address, "имеет координаты:", toponym_coodrinates)
+        else:
+            print("Ошибка выполнения запроса:")
+            print(zapros)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+
+
+    #
+    # def getImage(self):
+    #     map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+    #     response = requests.get(map_request)
+    #
+    #     if not response:
+    #         print("Ошибка выполнения запроса:")
+    #         print(map_request)
+    #         print("Http статус:", response.status_code, "(", response.reason, ")")
+    #         sys.exit(1)
+    #     self.map_file = "map.png"
+    #     with open(self.map_file, "wb") as file:
+    #         file.write(response.content)
+    #
+    # def closeEvent(self, event):
+    #     os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
