98db899abe49420c95823b82619af7ff24a759ad
dizonos <sobolevp58@gmail.com>
2022-02-08 11:11:18 +0300

    добавил обработку пейдж ап и пейдж даун

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import sys
-import os
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5 import uic
@@ -16,6 +16,19 @@ class MyWidget(QMainWindow):
         self.pushButton_3.clicked.connect(self.gibr)
         self.pushButton.clicked.connect(self.map)
         self.view = 'map'
+        self.z = 10
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_PageUp:
+            self.z += 1
+            if self.z > 17:
+                self.z = 17
+            self.search()
+        elif e.key() == Qt.Key_PageDown:
+            self.z -= 1
+            if self.z < 0:
+                self.z = 0
+            self.search()
 
     def search(self):
         zapros = f"https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={self.lineEdit.text()}1&format=json"
@@ -25,15 +38,12 @@ class MyWidget(QMainWindow):
             toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
             toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
             toponym_coodrinates = toponym["Point"]["pos"]
-            self.lineEdit_2.setText(toponym_address + " имеет координаты:" + toponym_coodrinates)
+            self.textEdit.setPlainText(toponym_address + " имеет координаты: \n" + toponym_coodrinates)
             map_request = 'http://static-maps.yandex.ru/1.x/'
             params = {
                 'll': ','.join(toponym_coodrinates.split(' ')),
-<<<<<<< HEAD
-                'l': self.view
-=======
-                'l': 'map'
->>>>>>> origin/master
+                'z': str(self.z),
+                'l': self.view,
             }
             response = requests.get(map_request, params=params)
             self.map_file = "map.png"
@@ -57,8 +67,13 @@ class MyWidget(QMainWindow):
         self.view = 'map'
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec_())
